d770246d1cdbc3b1dce6e96867051b36bb4fc347
astroxvimy <astroxvimy@gmail.com>
2025-03-28 00:18:28 +0900

    fix: import issue in test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { App } from './App';
+import App from './App';
 
 describe('App', () => {
   it('Renders hello world', () => {
